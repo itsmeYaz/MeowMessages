0b303712a17fcf90dbb10c8f77eb7a84f07edcff
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-19 23:15:20 +0800

    Add favicon and manifest file for better web experience

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ import {
   getDatabase,
   ref,
   push,
-  OnValue,
+  onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 //set database URL
@@ -19,19 +20,51 @@ const shoppingListDB = ref(database, "shoppingList");
 
 const inputEl = document.querySelector("#input-field");
 const addBtn = document.querySelector("#add-button");
-const ulEl = document.querySelector("#shopping-list");
+const shoppingList = document.querySelector("#shopping-list");
 
 addBtn.addEventListener("click", function () {
   let inputValue = inputEl.value;
   push(shoppingListDB, inputValue);
   clearInput();
-  appendItem(inputValue);
 });
+
+onValue(shoppingListDB, function (snapshot) {
+  if (snapshot.exists() === true) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingList();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItem(currentItem);
+    }
+  } else {
+    shoppingList.innerHTML = "No items yet. Wan't to add some?";
+  }
+});
+
+function clearShoppingList() {
+  shoppingList.innerHTML = "";
+}
 
 function clearInput() {
   inputEl.value = "";
 }
 
-function appendItem(input) {
-  ulEl.innerHTML += `<li>${input}</li>`;
+function appendItem(item) {
+  //   shoppingList.innerHTML += `<li>${input}</li>`;
+  let itemID = item[0];
+  let itemValue = item[1];
+  let newEl = document.createElement("li");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+    remove(exactLocationOfItemInDB);
+  });
+  shoppingList.append(newEl);
 }
